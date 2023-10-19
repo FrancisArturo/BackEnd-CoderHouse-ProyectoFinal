68c7951b3967fa92712937986c5f0f5e7ad42f13
Francis Arturo Alcalde <francisarturoalcaldesocole@gmail.com>
2023-10-19 20:26:59 -0300

    upload docs premium update

---
+++ src/repositories/Users.repository.js
@@ -0,0 +1,47 @@
+
+
+export default class UsersRepository {
+    constructor(dao) {
+        this.dao = dao;
+    }
+    getUserByEmail = async (user) => {
+        let result = await this.dao.getUserByEmailDao(user);
+        return result;
+    }
+    comparePsw = async (user) => {
+        let result = await this.dao.comparePswDao(user);
+        return result;
+    }
+    createUser = async (user) => {
+        let result = await this.dao.createUserDao(user);
+        return result;
+    }
+    loginUser = async (user) => {
+        let result = await this.dao.loginUserDao(user);
+        return result;
+    }
+    recoverPassword = async (user) => {
+        let result = await this.dao.recoverPasswordDao(user);
+        return result;
+    }
+    recoverCompletePsw = async (email, psw) => {
+        let result = await this.dao.recoverCompletePswDao(email, psw);
+        return result;
+    }
+    addDocumentUser = async (uid, doc) => {
+        let result = await this.dao.addDocumentUserDao(uid, doc);
+        return result;
+    }
+    updateUser = async (uid, userUpdate) => {
+        let result = await this.dao.updateUserDao(uid, userUpdate);
+        return result;
+    }
+    getUserById = async (uid) => {
+        let result = await this.dao.getUserByIdDao(uid);
+        return result;
+    }
+    deleteUserById = async (uid) => {
+        let result = await this.dao.deleteUserByIdDao(uid);
+        return result;
+    }
+}
---
+++ src/routes/session.routes.js
@@ -0,0 +1,34 @@
+import { Router } from "express";
+import passport from "passport";
+import handlePolicies from "../middleware/handle-police.middleware.js";
+import SessionController from "../controllers/session.controller.js";
+import { uploader } from "../middleware/uploader.middleware.js";
+
+
+
+
+export default class sessionRoutes {
+    path = "/session";
+    router = Router();
+    sessionController = new SessionController();
+
+    constructor() {
+        this.initializeRoutes();
+    }
+
+    initializeRoutes() {
+        this.router.post(`${this.path}/register`, this.sessionController.createUserController);
+        this.router.post(`${this.path}/login`, this.sessionController.loginUserController);
+        this.router.get(`${this.path}/logout`, this.sessionController.logoutUserController);
+        this.router.post(`${this.path}/recover`, this.sessionController.recoverPasswordController);
+        this.router.get(`${this.path}/premium/:uid`, this.sessionController.updateUserRoleController);
+        this.router.post(`${this.path}/:uid/documents`, uploader.array('file'), this.sessionController.uploadDocumentController);
+        this.router.post(`${this.path}/recover/complete`, handlePolicies(["pswRecover"]), this.sessionController.recoverCompletePswController);
+        this.router.get(`${this.path}/user`, handlePolicies(["admin", "user", "premium"]), this.sessionController.getUserByIdController);
+        this.router.get(`${this.path}/github`, passport.authenticate("github", { scope: [ 'user:email' ], session: false}));
+        this.router.get(`${this.path}/github/callback`, passport.authenticate("github", { failureRedirect: "/api/v1/session/failedlogin", session: false }), this.sessionController.githubLoginController);
+        this.router.get(`${this.path}/current`,  handlePolicies(["admin", "user", "premium"]), this.sessionController.currentController);
+        this.router.delete(`${this.path}/user/:uid`, this.sessionController.deleteUserByIdController);
+    }
+}
+
